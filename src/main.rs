cdcefca0fb33a0263d802e5323d164af91317662
Sriharsha Mucheli <harshasrisri@gmail.com>
2020-03-10 15:58:26 -0700

    fix some error handling

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,28 @@ use split::Splitter;
 
 fn main() {
     let args = CrustOpts::from_args();
-    let parser =
-        FieldParser::from_spec(&args.fields, args.complement).expect("Error parsing fields");
+    let parser = FieldParser::from_spec(&args.fields, args.complement);
+    if parser.is_err() {
+        eprintln!("Failed to parse fields");
+        std::process::exit(1);
+    }
+    let parser = parser.unwrap();
     let splitter = Splitter::new(&parser, args.delimiter, args.separator);
     let mut output_line = String::new();
 
     for file in args.files {
         let reader: Box<dyn BufRead> = match file.to_str() {
             Some("-") => Box::new(BufReader::new(io::stdin())),
-            Some(path) => {
+            Some(_) => {
                 if !file.exists() && !file.is_file() {
                     eprintln!("Invalid file: {}", file.to_str().unwrap_or_default());
                     std::process::exit(1);
                 }
 
-                Box::new(BufReader::new(File::open(path).unwrap()))
+                Box::new(BufReader::new(File::open(file).unwrap()))
             }
             None => {
-                eprintln!("Bad PathBuf");
+                eprintln!("Bad filename - {}", file.display());
                 std::process::exit(1);
             }
         };
