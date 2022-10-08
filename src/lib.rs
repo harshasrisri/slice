8fd7229ff01b7a395b9bf05175341dca6ed764b7
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-10-08 02:21:35 -0700

    Fearless refactor: move field spec to separate library

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod args;
-pub mod fields;
-pub mod split;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,109 @@
-use slice::args::SliceOpts;
-use slice::fields::FieldSpecParser;
-use slice::split::Splitter;
+use std::fmt::Write;
+use field_spec::FieldSpecParser;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::char::ParseCharError;
+use std::path::PathBuf;
+use std::str::FromStr;
 use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    about,
+    after_help = "
+FIELD SPECIFICATION:
+    The required fileds to be extracted can be specified or combined like below:
+        3           => Extract column 3
+        4-7         => Extract fields 4,5,6,7
+        -5          => Extract all fields upto and including 5, i.e 1,2,3,4,5
+        6-          => Extract all fields from and including 6, ie. 6,7,8,...
+        2,4,6       => Extract only fields 2, 4 and 6
+        -2,5-7,9-   => Extract fields 1,2,5,6,7,9,...
+"
+)]
+pub struct SliceOpts {
+    /// Fields to be extracted. See FIELD SPECIFICATION
+    #[structopt(short, long, allow_hyphen_values = true)]
+    pub fields: String,
+
+    /// Start index of field separation
+    #[structopt(short = "S", long, default_value = "0")]
+    pub start_index: usize,
+
+    /// Interval  separator in field specification
+    #[structopt(short, long, default_value = ",")]
+    pub interval_separator: String,
+
+    /// Range  separator in field specification
+    #[structopt(short, long, default_value = "-")]
+    pub range_separator: String,
+
+    /// Rows to be extracted. All, by default. See FIELD SPECIFICATION
+    #[structopt(short, long, allow_hyphen_values = true)]
+    pub rows: Option<String>,
+
+    /// Delimiter to be used to split fields
+    #[structopt(short, long, parse(try_from_str = parse_for_tab), default_value = " ")]
+    pub delimiter: char,
+
+    /// Separator to use to print results
+    #[structopt(short, long, default_value = " ")]
+    pub separator: String,
+
+    /// Include lines that don't contain a delimiter
+    #[structopt(short, long)]
+    pub non_delimited: bool,
+
+    /// Complement field spec. Print all fields but those specified with -f
+    #[structopt(short, long)]
+    pub complement: bool,
+
+    /// Files to process
+    #[structopt(name = "FILES", parse(from_os_str), default_value = "-")]
+    pub files: Vec<PathBuf>,
+}
+
+fn parse_for_tab(src: &str) -> Result<char, ParseCharError> {
+    if src == "\\t" {
+        Ok('\t')
+    } else {
+        char::from_str(src)
+    }
+}
+
+#[derive(Debug)]
+pub struct Splitter {
+    fields: FieldSpecParser,
+    delimiter: char,
+    separator: String,
+}
+
+impl Splitter {
+    pub fn new(fields: FieldSpecParser, delimiter: char, separator: String) -> Self {
+        Splitter {
+            fields,
+            delimiter,
+            separator,
+        }
+    }
+
+    pub fn parse_into<T>(&self, input: &str, mut output: &mut T) -> Result<(), std::fmt::Error>
+        where
+        T: Write,
+        {
+            write!(
+                &mut output,
+                "{}",
+                input
+                .split(self.delimiter)
+                .filter(|&s| !s.is_empty())
+                .zip(self.fields.mask_iter())
+                .filter_map(|(field, allow)| if allow { Some(field) } else { None })
+                .collect::<Vec<_>>()
+                .join(&self.separator)
+                )
+        }
+}
 
 fn main() {
     let args = SliceOpts::from_args();
